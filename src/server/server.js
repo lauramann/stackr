f4092c06eedfee801760a2fc1d10bc48cb1637ca
lauramann <laura@graindiscovery.com>
2020-06-04 20:35:14 -0400

    FIXED BY ADDING IF TO CHECK IF OFFERS HAS ANYTHING BEFORE SENDING

--- src/server/server.js
+++ src/server/server.js
@@ -1,33 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const checkout51 = require('./api/checkout51');
-const flipp = require('./api/flipp');
+const checkout51 = require("./api/checkout51");
+const flipp = require("./api/flipp");
 
-
-
-app.get('/getOffers', function(req, res) {
-    let offers = []
-    checkout51().then(data => {
+app.get("/getOffers", function(req, res) {
+    checkout51().then((data) => {
+        let offers = [];
         data.forEach((offer) => {
-                // console.log('OFFER', offer);
-                // console.log(callFlipp(offer, req.query.postalCode));
-                flipp(offer.offer_name, req.query.postalCode).then(data => {
-                    // console.log('FLIPP DEALS', data);
-                    // res.send(data);
-                    // return { offer, flippItems: data.items }
-                    offers.push({ offer, flippItems: data.items })
-                })
+            flipp(offer.offer_name, req.query.postalCode).then((flyer) => {
+                offers.push({ offer, flippItems: flyer.items });
+                if (offers.length === data.length) {
+                    res.send(offers);
+                }
+            });
+        });
+    });
+});
 
-            }).then(() => {
-                console.log(offers)
-                res.send(offers)
-            })
-            // res.send(data);
-    })
-})
-
-
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.listen("8081");
+console.log("Magic happens on port 8081");
 exports = module.exports = app;
